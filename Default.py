6e84d82dbb3766074d99ebf2cdfc5330f31e1e26
nvfal <nvfallen@gmail.com>
2022-08-24 13:58:30 +0300

    Добавлены настройки по умолчанию, кнопка меню о нас

---
+++ Default.py
@@ -0,0 +1,135 @@
+import json
+import os
+import pathlib
+import default_window
+
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QLineEdit, QDialog, QButtonGroup, QLabel, QSizePolicy, QPushButton
+
+
+class Button(QLineEdit):
+
+    def __init__(self, parent):
+        super(Button, self).__init__(parent)
+
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, e):
+
+        if e.mimeData().hasUrls():
+            e.accept()
+        else:
+            super(Button, self).dragEnterEvent(e)
+
+    def dragMoveEvent(self, e):
+
+        super(Button, self).dragMoveEvent(e)
+
+    def dropEvent(self, e):
+
+        if e.mimeData().hasUrls():
+            for url in e.mimeData().urls():
+                self.setText(os.path.normcase(url.toLocalFile()))
+                e.accept()
+        else:
+            super(Button, self).dropEvent(e)
+
+
+class DefaultWindow(QDialog, default_window.Ui_Dialog):  # Настройки по умолчанию
+    def __init__(self, parent):
+        super().__init__()
+        self.setupUi(self)
+        self.parent = parent
+        # Имена
+        self.name_list = {'checked-path_check': 'Путь к файлам', 'checked-table_number': 'Номер таблицы',
+                          'checked-stationary_FSB': 'Стац. ФСБ', 'checked-carry_FSB': 'Воз. ФСБ',
+                          'checked-wear_FSB': 'Нос. ФСБ', 'checked-r1_FSB': 'r1 ФСБ', 'checked-r1s_FSB': 'r1` ФСБ',
+                          'checked-stationary_FSTEK': 'Стац. ФСТЭК', 'checked-carry_FSTEK': 'Воз. ФСТЭК',
+                          'checked-wear_FSTEK': 'Нос. ФСТЭК', 'checked-r1_FSTEK': 'r1 ФСТЭК',
+                          'parser-path_parser': 'Путь к файлам',
+                          'extract-path_original_extract': 'Путь к файлам', 'extract-conclusion': 'ФИО заключение',
+                          'extract-protocol': 'ФИО протокол', 'extract-prescription': 'ФИО предписание',
+                          'gen_pemi-path_original_file': 'Путь к исходникам',
+                          'gen_pemi-path_finish_folder': 'Папка для генерации',
+                          'gen_pemi-path_freq_restrict': 'Файл ограничений',
+                          'gen_pemi-complect_quant_pemi': 'Количество комплектов',
+                          'gen_pemi-complect_number_pemi': 'Номера комплектов',
+                          'HFE-path_file_HFE': 'Путь к файлам', 'HFE-complect_quant_HFE': 'Количество комплектов',
+                          'HFE-frequency': 'Частота', 'HFE-level': 'Уровень',
+                          'HFI-path_file_HFI': 'Путь к файлам', 'HFI-complect_quant_HFI': 'Количество комплектов',
+                          'HFI-imposition_freq': 'Частота навязывания'}
+        self.name_box = [self.groupBox_checked, self.groupBox_parcing, self.groupBox_exctracting,
+                         self.groupBox_gen_pemi, self.groupBox_gen_hfe, self.groupBox_gen_hfi]
+        self.name_grid = [self.gridLayout_checked, self.gridLayout_parcer, self.gridLayout_exctract,
+                          self.gridLayout_gen_pemi, self.gridLayout_HFE, self.gridLayout_HFI]
+        with open(pathlib.Path(pathlib.Path.cwd(), 'Настройки.txt'), "r", encoding='utf-8-sig') as f:  # Открываем
+            self.data = json.load(f)  # Загружаем данные
+        self.buttongroup_add = QButtonGroup()
+        self.buttongroup_add.buttonClicked[int].connect(self.add_button_clicked)
+        self.pushButton_ok.clicked.connect(self.accept)  # Принять
+        self.pushButton_cancel.clicked.connect(lambda: self.close())  # Отмена
+        self.line = {}  # Для имен
+        self.name = {}  # Для значений
+        self.button = {}  # Для кнопки «изменить»
+        for i, el in enumerate(self.name_list):  # Заполняем
+            frame = False
+            grid = False
+            for j, n in enumerate(['checked', 'parser', 'extract', 'gen_pemi', 'HFE', 'HFI']):
+                if n in el.partition('-')[0]:
+                    frame = self.name_box[j]
+                    grid = self.name_grid[j]
+                    break
+            self.line[i] = QLabel(frame)  # Помещаем в фрейм
+            self.line[i].setText(self.name_list[el])  # Название элемента
+            self.line[i].setFont(QFont("Times", 12, QFont.Light))  # Шрифт, размер
+            self.line[i].setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)  # Размеры виджета
+            self.line[i].setFixedWidth(220)
+            self.line[i].setDisabled(True)  # Делаем неактивным, чтобы нельзя было просто так редактировать
+            grid.addWidget(self.line[i], i, 0)  # Добавляем виджет
+            self.name[i] = Button(frame)  # Помещаем в фрейм
+            if el in self.data:
+                self.name[i].setText(self.data[el])
+            self.name[i].setFont(QFont("Times", 12, QFont.Light))  # Шрифт, размер
+            self.name[i].setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)  # Размеры виджета
+            self.name[i].setStyleSheet("QLineEdit {"
+                                       "border-style: solid;"
+                                       "}")
+            self.name[i].setDisabled(True)  # Неактивный
+            grid.addWidget(self.name[i], i, 1)  # Помещаем в фрейм
+            self.button[i] = QPushButton("Изменить", frame)  # Создаем кнопку
+            self.button[i].setFont(QFont("Times", 12, QFont.Light))  # Размер шрифта
+            self.button[i].setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)  # Размеры виджета
+            self.buttongroup_add.addButton(self.button[i], i)  # Добавляем в группу
+            grid.addWidget(self.button[i], i, 2)  # Добавляем в фрейм по месту
+
+    def add_button_clicked(self, number):  # Если кликнули по кнопке
+        self.name[number].setEnabled(True)  # Делаем активным для изменения
+        self.name[number].setStyleSheet("QLineEdit {"
+                                        "border-style: solid;"
+                                        "border-width: 1px;"
+                                        "border-color: black; "
+                                        "}")
+
+    def accept(self):  # Если нажали кнопку принять
+        for i, el in enumerate(self.name_list):  # Пробегаем значения
+            if self.name[i].isEnabled():  # Если виджет активный (означает потенциальное изменение)
+                if self.name[i].text():  # Если внутри виджета есть текст, то помещаем внутрь базы
+                    self.data[el] = self.name[i].text()
+                else:  # Если нет текста, то удаляем значение
+                    self.data[el] = None
+        with open(pathlib.Path(pathlib.Path.cwd(), 'Настройки.txt'), 'w', encoding='utf-8-sig') as f:  # Пишем в файл
+            json.dump(self.data, f, ensure_ascii=False, sort_keys=True, indent=4)
+        self.close()  # Закрываем
+
+    def closeEvent(self, event):
+        os.chdir(pathlib.Path.cwd())
+        if self.sender() and self.sender().text() == 'Принять':
+            event.accept()
+            with open(pathlib.Path(pathlib.Path.cwd(), 'Настройки.txt'), "r", encoding='utf-8-sig') as f:  # Открываем
+                data = json.load(f)  # Загружаем данные
+            self.parent.default_date(data)
+            self.parent.show()
+        else:
+            event.accept()
+            self.parent.show()
+
--- OOD.py
+++ OOD.py
@@ -1,3 +1,5 @@
+import json
+import pathlib
 import queue
 import sys
 
@@ -6,15 +8,28 @@ from PyQt5.QtGui import QSyntaxHighlighter, QTextCharFormat, QColor
 import Main
 import logging
 from PyQt5.QtCore import (QTranslator, QLocale, QLibraryInfo, QDir)
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QFileDialog, QMessageBox)
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QFileDialog, QMessageBox, QDialog)
 from checked import (checked_zone_checked, checked_file_parcing, check_generation_data, checked_delete_header_footer,
                      checked_hfe_generation, checked_hfi_generation)
+import about
+from Default import DefaultWindow
 from Zone_Check import ZoneChecked
 from File_Parcing import FileParcing
 from Generation_Files import GenerationFile
 from Delete_Header_Footer import DeleteHeaderFooter
 from HFE_Generation import HFEGeneration
 from HFI_Generation import HFIGeneration
+
+
+class AboutWindow(QDialog, about.Ui_Dialog):  # Для отображения информации
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+def about():  # Открываем окно с описанием
+    window_add = AboutWindow()
+    window_add.exec_()
 
 
 class SyntaxHighlighter(QSyntaxHighlighter):
@@ -75,6 +90,59 @@ class MainWindow(QMainWindow, Main.Ui_MainWindow):  # Главное окно
         self.pushButton_generation_exctract.clicked.connect(self.delete_header_footer)
         self.pushButton_generation_HFE.clicked.connect(self.generate_hfe)
         self.pushButton_generation_HFI.clicked.connect(self.generate_hfi)
+        self.action_settings_default.triggered.connect(self.default_settings)
+        self.menu_about.aboutToShow.connect(about)
+
+        self.path_for_default = pathlib.Path.cwd()  # Путь для файла настроек
+        # Имена в файле
+        self.name_list = {'checked-path_check': 'Путь к файлам', 'checked-table_number': 'Номер таблицы',
+                          'checked-stationary_FSB': 'Стац. ФСБ', 'checked-carry_FSB': 'Воз. ФСБ',
+                          'checked-wear_FSB': 'Нос. ФСБ', 'checked-r1_FSB': 'r1 ФСБ', 'checked-r1s_FSB': 'r1` ФСБ',
+                          'checked-stationary_FSTEK': 'Стац. ФСТЭК', 'checked-carry_FSTEK': 'Воз. ФСТЭК',
+                          'checked-wear_FSTEK': 'Нос. ФСТЭК', 'checked-r1_FSTEK': 'r1 ФСТЭК',
+                          'parser-path_parser': 'Путь к файлам',
+                          'extract-path_original_extract': 'Путь к файлам', 'extract-conclusion': 'ФИО заключение',
+                          'extract-protocol': 'ФИО протокол', 'extract-prescription': 'ФИО предписание',
+                          'gen_pemi-path_original_file': 'Путь к начальным файлам',
+                          'gen_pemi-path_finish_folder': 'Путь к конечным файлам',
+                          'gen_pemi-path_freq_restrict': 'Файл ограничений',
+                          'gen_pemi-complect_quant_pemi': 'Количество комплектов',
+                          'gen_pemi-complect_number_pemi': 'Номера комплектов',
+                          'HFE-path_file_HFE': 'Путь к файлам', 'HFE-complect_quant_HFE': 'Количество комплектов',
+                          'HFE-frequency': 'Частота', 'HFE-level': 'Уровень',
+                          'HFI-path_file_HFI': 'Путь к файлам', 'HFI-complect_quant_HFI': 'Количество комплектов',
+                          'HFI-imposition_freq': 'Частота навязывания'}
+        # Грузим значения по умолчанию
+        try:
+            with open(pathlib.Path(pathlib.Path.cwd(), 'Настройки.txt'), "r", encoding='utf-8-sig') as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            with open(pathlib.Path(pathlib.Path.cwd(), 'Настройки.txt'), "w", encoding='utf-8-sig') as f:
+                json.dump({}, f, ensure_ascii=False, sort_keys=True, indent=4)
+                data = {}
+
+        # Линии для заполнения
+        self.line = [self.lineEdit_path_check, self.lineEdit_table_number, self.lineEdit_stationary_FSB,
+                     self.lineEdit_carry_FSB, self.lineEdit_wear_FSB, self.lineEdit_r1_FSB, self.lineEdit_r1s_FSB,
+                     self.lineEdit_stationary_FSTEK, self.lineEdit_carry_FSTEK, self.lineEdit_wear_FSTEK,
+                     self.lineEdit_r1_FSTEK, self.lineEdit_path_parser, self.lineEdit_path_original_file,
+                     self.lineEdit_path_finish_folder, self.lineEdit_path_freq_restrict,
+                     self.lineEdit_complect_quant_pemi, self.lineEdit_complect_number_pemi,
+                     self.lineEdit_path_file_HFE, self.lineEdit_complect_quant_HFE, self.lineEdit_frequency,
+                     self.lineEdit_level, self.lineEdit_path_file_HFI, self.lineEdit_complect_quant_HFI,
+                     self.lineEdit_imposition_freq, self.lineEdit_path_original_extract, self.lineEdit_conclusion,
+                     self.lineEdit_protocol, self.lineEdit_prescription]
+        self.default_date(data)
+
+    def default_date(self, d):
+        for i, el in enumerate(self.name_list):
+            if el in d:
+                self.line[i].setText(d[el])  # Помещаем значение
+
+    def default_settings(self):  # Запускаем окно с настройками по умолчанию.
+        self.close()
+        window_add = DefaultWindow(self)
+        window_add.show()
 
     def group_box_change_state(self, state):
         if self.sender() == self.groupBox_FSTEK and state:
